3eb1711bfd93650cf0718d095de13e0862a3ad4e
Tiziano Santoro <tiziano88@gmail.com>
2016-02-12 18:12:14 +0000

    skeleton

--- server/main.go
+++ server/main.go
@@ -1,15 +1,30 @@
-package server
+package main
 
 import (
+	"net/http"
+
 	"github.com/golang/protobuf/jsonpb"
+	"github.com/gorilla/pat"
+
+	pb "github.com/tiziano88/gelm/server/proto"
 )
 
 var marshaler = &jsonpb.Marshaler{
-	EnumAsInts:   false,
+	EnumsAsInts:  false,
 	EmitDefaults: true,
 	Indent:       "  ",
 	OrigName:     true,
 }
 
 func main() {
+	m := pat.New()
+
+	m.Get("/", Handler)
+
+	http.Handle("/", m)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	m := &pb.Message{}
+	marshaler.Marshal(w, m)
 }
